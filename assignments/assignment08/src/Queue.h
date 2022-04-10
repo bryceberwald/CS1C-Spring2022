bf7f02323810748213fa2b9c336d2365d7096676
bryceberwald <bryce@berwald.io>
2022-04-09 19:50:21 -0700

    Finished Assignment #8

--- assignments/assignment08/src/Queue.h
+++ assignments/assignment08/src/Queue.h
@@ -11,45 +11,137 @@
 #include <iostream>
 #include <string>
 
-const int MAX_QUEUE_SIZE = 12;
+const int MAX_QUEUE_SIZE = 12;     // Variable used to determine max array size.
 
-template <class data_type>
+template <class data_type>         // Template for Queue class.
+
+/***************************************************
+ * Queue Class
+ **************************************************/
 class Queue {
     public:
+        /**************************************************
+        * Default Constructor
+        * No Parameters
+        **************************************************/
         Queue();
+
+        /**************************************************
+        * Non-Default Constructor
+        * 1 Parameter (Queue Object passed by reference)
+        **************************************************/
+        Queue(Queue& copyQueue);
+
+        /**************************************************
+        * Destructor
+        * No Parameters
+        **************************************************/
         ~Queue();
+
+        /**************************************************
+        * This is the Queue class enqueuing function. A
+        * queue is a LIFO data structure. This function
+        * takes one parameter of a to be determined data
+        * type. This function is of type void and does not
+        * return anything.
+        **************************************************/
         void Enqueue(data_type data);
 
-        void Dequeue(data_type data);
+        /**************************************************
+        * This is the Queue class dequeuing function. A
+        * queue is a LIFO data structure. This function
+        * has no parameters and is of type void, so nothing
+        * is returned.
+        **************************************************/
+        void Dequeue();
 
-        data_type Front();
+        /**************************************************
+        * This is the Queue class function for returning
+        * the front item in the queue. The Queue class uses
+        * a template, so the determined data type for the
+        * queue will be used for returning the front item.
+        **************************************************/
+        data_type Front()const;
 
-        data_type Size();
+        /**************************************************
+        * This is the Queue class function for returning
+        * the current size of the queue. This function is
+        * of type integer and the same data type will be
+        * returned.
+        **************************************************/
+        int Size()const;
 
-        data_type IsEmpty();
+        /**************************************************
+        * This is the Queue class function for checking if
+        * the queue is currently empty. This function is of
+        * type boolean, true will be returned fort empty
+        * and false will be returned otherwise.
+        **************************************************/
+        bool IsEmpty()const;
 
-        data_type IsFull();
+        /**************************************************
+        * This is the Queue class function for checking if
+        * the queue is currently full. This function is of
+        * type boolean, true will be returned for full and
+        * false will be returned otherwise.
+        **************************************************/
+        bool IsFull()const;
 
+        /**************************************************
+        * This is the Queue class function for printing all
+        * items currently in the queue. This function is of
+        * type void and nothing will be returned.
+        **************************************************/
         void PrintQueue()const;
-        
-    private:
-        int currentSize;
-        data_type QueueArr[MAX_QUEUE_SIZE];
 
+        /**************************************************
+        * This is the Queue class function for getting a
+        * queue value with the index being passed as the
+        * parameter. This function will return the
+        * determined data type for the array value.
+        **************************************************/
+        data_type getQueueArrayValue(int index)const;
+
+    private:
+        int currentSize;                      // Variable holds the current size of the queue/queueArr.
+        int topIndex;                         // Variable holds the head-index of the queue.
+        int bottomIndex;                      // Variable holds the tail-index of the queue.
+        data_type QueueArr[MAX_QUEUE_SIZE];   // Array for holding all of the array items of Max Size of 12.
 };
 
+
 /***********************************************************
- * 
+ * This is the default constructor for the Queue class. This
+ * function will initialize the needed attributes for a
+ * Queue.
  ***********************************************************/
 template <class data_type>
 Queue<data_type>::Queue(){
     this->currentSize = 0;
-    std::cout << "\n\nQueue Class Working!!!\n\n";
+    this->topIndex = -1;
+    this->bottomIndex = -1;
+    std::cout << "\nQueue Class Template has been Initialized!\n";
 }
 
 
 /***********************************************************
- * 
+ * This is the non-default copy constructor for the Queue
+ * class. This constructor takes a Queue object as a parameter
+ * being passed by reference to copy all of it's list elements
+ * to the current queue.
+ ***********************************************************/
+template <class data_type>
+Queue<data_type>::Queue(Queue& copyQueue){
+    this->currentSize = copyQueue.Size();
+    for(int i = 0; i < currentSize; i++){
+        this->QueueArr[i] = copyQueue.getQueueArrayValue(i);
+    }
+}
+
+
+/***********************************************************
+ * This is the destructor for the queue class. This function
+ * will be called when the queue class goes out of scope.
  ***********************************************************/
 template <class data_type>
 Queue<data_type>::~Queue(){
@@ -58,68 +150,129 @@ Queue<data_type>::~Queue(){
 
 
 /***********************************************************
- * 
+ * This is the Queue class function for adding a item to
+ * the queue. A queue is a LIFO data structure, so we will be
+ * adding to the end of the queue. This function is of type 
+ * void and does not return anything.
  ***********************************************************/
 template <class data_type>
 void Queue<data_type>::Enqueue(data_type item){
-    this->QueueArr[this->currentSize] = item;
-    this->currentSize++;
+    if(currentSize != MAX_QUEUE_SIZE){
+        if(currentSize == 0){
+            this->topIndex = 0;
+            this->bottomIndex = 0;
+            this->QueueArr[bottomIndex] = item;
+        } else {
+            this->bottomIndex = this->currentSize;
+            this->QueueArr[bottomIndex] = item;
+        }
+        this->currentSize++;
+    }
 }
 
 
 /***********************************************************
- * 
+ * This is the Queue class function for removing an item
+ * from the queue. A queue is a LIFO data structure, and
+ * the item will be removed from the front. This function is
+ * of type void and does not return anything.
  ***********************************************************/
 template <class data_type>
-void Queue<data_type>::Dequeue(data_type item){
+void Queue<data_type>::Dequeue(){
+    if(this->currentSize != 0){
+        for(int i = 0; i < this->currentSize; i++){
+            this->QueueArr[i] = this->QueueArr[i+1];
+        }
+        this->currentSize--;
 
+        if(this->currentSize == 0){
+            topIndex = -1;
+            bottomIndex = -1;
+        }
+    }
 }
 
 
 /***********************************************************
- * 
+ * This is the Queue class function for returning the front
+ * item of the queue of to be determined data type thanks to
+ * the class template. 
  ***********************************************************/
 template <class data_type>
-data_type Queue<data_type>::Front(){
-
+data_type Queue<data_type>::Front()const{
+    if(currentSize != 0){
+        return this->QueueArr[topIndex];
+    }
 }
 
 
 /***********************************************************
- * 
+ * This is the Queue class function for returning the current
+ * size of the array. This function is of type integer and
+ * the same data type will be returned.
  ***********************************************************/
 template <class data_type>
-data_type Queue<data_type>::Size(){
-
+int Queue<data_type>::Size()const{
+    return this->currentSize;
 }
 
 
 /***********************************************************
- * 
+ * This is the Queue class function for checking if the 
+ * queue is currently empty. This function is of type bool
+ * and will return true for a empty queue and will return
+ * false when the queue is not empty.
  ***********************************************************/
 template <class data_type>
-data_type Queue<data_type>::IsEmpty(){
-
+bool Queue<data_type>::IsEmpty()const{
+    if(this->currentSize == 0){
+        return true;
+    } else {
+        return false;
+    }
 }
 
 
 /***********************************************************
- * 
+ * This is the Queue class function for checking if the queue
+ * is currently full or not. This function is of type bool
+ * and will return true if the queue is full and false
+ * otherise.
  ***********************************************************/
 template <class data_type>
-data_type Queue<data_type>::IsFull(){
-
+bool Queue<data_type>::IsFull()const{
+    if(this->currentSize == MAX_QUEUE_SIZE){
+        return true;
+    } else {
+        return false;
+    }
 }
 
 
 /***********************************************************
- * 
+ * This is the Queue class function for printing all the
+ * current queue values to the console. This function is of
+ * type void and nothing is returned.
  ***********************************************************/
 template <class data_type>
 void Queue<data_type>::PrintQueue()const{
+    std::cout << "\nCURRENT QUEUE: \n";
     for(int i = 0; i < this->currentSize; i++){
         std::cout << this->QueueArr[i] << std::endl;
     }
 }
+
+
+/***********************************************************
+ * This is the Queue class function for getting a value from
+ * the queue at the index being passed as the parameter.
+ * This function is of a to be determined data type, which
+ * will be returned for the queue's value to be obtained.
+ ***********************************************************/
+template <class data_type>
+data_type Queue<data_type>::getQueueArrayValue(int index) const{
+    return this->QueueArr[index];
+}
+
 
 #endif
--- assignments/assignment08/src/main.cpp
+++ assignments/assignment08/src/main.cpp
@@ -11,21 +11,262 @@
 
 int main(void) {
 
+    // Call function to print the class header to the console.
     PrintClassHeader(cout);
 
+    // STL Queue Template Example.
     std::queue<int> STLqueue;
-
     STLqueue.push(10);
     STLqueue.push(10);
     STLqueue.push(10);
-    cout << STLqueue.size();
+    cout << "\nThe size of the STL Queue is: " << STLqueue.size() << endl;
 
-    Queue <int>myQueue;
+    // Create a new Queue of Char data type.
+    Queue <char>QueueChar;
 
-    myQueue.Enqueue(7);
-    myQueue.Enqueue(77);
-    myQueue.Enqueue(777);
-    myQueue.PrintQueue();
+    // Test IsEmpty() class function. (PASS CONDITION)
+    if(QueueChar.IsEmpty()){
+        cout << "\nQueue is EMPTY!!";
+        cout << "\nThe Current size is: " << QueueChar.Size() << endl;
+    } else {
+        cout << "\nQueue is NOT EMPTY!";
+        cout << "\nThe Current size is: " << QueueChar.Size() << endl;
+    }
+
+    // Char Enqueue's (6 Total)
+    QueueChar.Enqueue('a');
+    QueueChar.PrintQueue();
+
+    // Test IsEmpty() class function. (FAIL CONDITION)
+    if(QueueChar.IsEmpty()){
+        cout << "\nQueue is EMPTY!!";
+        cout << "\nThe Current size is: " << QueueChar.Size() << endl;
+    } else {
+        cout << "\nQueue is NOT EMPTY!";
+        cout << "\nThe Current size is: " << QueueChar.Size() << endl;
+    }
+
+    QueueChar.Enqueue('b');
+    QueueChar.PrintQueue();
+
+    // Display the front most item in queue.
+    cout << "\nThe Front item is: " << QueueChar.Front() << endl;
+
+    QueueChar.Enqueue('c');
+    QueueChar.PrintQueue();
+    QueueChar.Enqueue('d');
+    QueueChar.PrintQueue();
+    QueueChar.Enqueue('e');
+    QueueChar.PrintQueue();
+    QueueChar.Enqueue('f');
+    QueueChar.PrintQueue();
+
+    // Char Dequeue's (3 Total)
+    QueueChar.Dequeue();
+    QueueChar.PrintQueue();
+    QueueChar.Dequeue();
+    QueueChar.PrintQueue();
+    QueueChar.Dequeue();
+    QueueChar.PrintQueue();
+
+    // Char Enqueue's (5 Total)
+    QueueChar.Enqueue('g');
+    QueueChar.PrintQueue();
+    QueueChar.Enqueue('h');
+    QueueChar.PrintQueue();
+    QueueChar.Enqueue('i');
+    QueueChar.PrintQueue();
+    QueueChar.Enqueue('j');
+    QueueChar.PrintQueue();
+    QueueChar.Enqueue('k');
+    QueueChar.PrintQueue();
+
+    // Char Dequeue's (6 Total)
+    QueueChar.Dequeue();
+    QueueChar.PrintQueue();
+    QueueChar.Dequeue();
+    QueueChar.PrintQueue();
+    QueueChar.Dequeue();
+    QueueChar.PrintQueue();
+    QueueChar.Dequeue();
+    QueueChar.PrintQueue();
+    QueueChar.Dequeue();
+    QueueChar.PrintQueue();
+    QueueChar.Dequeue();
+
+    // Create a new Queue of Integer data type.
+    Queue <int>QueueInt;
+
+    // Test IsEmpty() class function. (PASS CONDITION)
+    if(QueueInt.IsEmpty()){
+        cout << "\nQueue is EMPTY!!";
+        cout << "\nThe Current size is: " << QueueInt.Size() << endl;
+    } else {
+        cout << "\nQueue is NOT EMPTY!";
+        cout << "\nThe Current size is: " << QueueInt.Size() << endl;
+    }
+
+    // Int Enqueue's (6 Total)
+    QueueInt.Enqueue(1);
+    QueueInt.PrintQueue();
+
+    // Test IsEmpty() class function. (FAIL CONDITION)
+    if(QueueInt.IsEmpty()){
+        cout << "\nQueue is EMPTY!!";
+        cout << "\nThe Current size is: " << QueueInt.Size() << endl;
+    } else {
+        cout << "\nQueue is NOT EMPTY!";
+        cout << "\nThe Current size is: " << QueueInt.Size() << endl;
+    }
+
+    QueueInt.Enqueue(2);
+    QueueInt.PrintQueue();
+
+    // Display the front most item in queue.
+    cout << "\nThe Front item is: " << QueueInt.Front() << endl;
+
+    QueueInt.Enqueue(3);
+    QueueInt.PrintQueue();
+    QueueInt.Enqueue(4);
+    QueueInt.PrintQueue();
+    QueueInt.Enqueue(5);
+    QueueInt.PrintQueue();
+    QueueInt.Enqueue(6);
+    QueueInt.PrintQueue();
+
+    // Int Dequeue's (4 Total)
+    QueueInt.Dequeue();
+    QueueInt.PrintQueue();
+    QueueInt.Dequeue();
+    QueueInt.PrintQueue();
+    QueueInt.Dequeue();
+    QueueInt.PrintQueue();
+    QueueInt.Dequeue();
+    QueueInt.PrintQueue();
+
+    // Int Enqueue's (3 Total)
+    QueueInt.Enqueue(7);
+    QueueInt.PrintQueue();
+    QueueInt.Enqueue(8);
+    QueueInt.PrintQueue();
+    QueueInt.Enqueue(9);
+    QueueInt.PrintQueue();
+
+    // Int Dequeue's (3 Total)
+    QueueInt.Dequeue();
+    QueueInt.PrintQueue();
+    QueueInt.Dequeue();
+    QueueInt.PrintQueue();
+    QueueInt.Dequeue();
+    QueueInt.PrintQueue();
+    
+    // Create a new Queue of Double data type.
+    Queue <double>QueueDouble;
+
+    // Test IsEmpty() class function. (PASS CONDITION)
+    if(QueueDouble.IsEmpty()){
+        cout << "\nQueue is EMPTY!!";
+        cout << "\nThe Current size is: " << QueueDouble.Size() << endl;
+    } else {
+        cout << "\nQueue is NOT EMPTY!";
+        cout << "\nThe Current size is: " << QueueDouble.Size() << endl;
+    }
+
+    // Double Enqueue's (7 Total)
+    QueueDouble.Enqueue(1.1);
+    QueueDouble.PrintQueue();
+
+    // Test IsEmpty() class function. (FAIL CONDITION)
+    if(QueueDouble.IsEmpty()){
+        cout << "\nQueue is EMPTY!!";
+        cout << "\nThe Current size is: " << QueueDouble.Size() << endl;
+    } else {
+        cout << "\nQueue is NOT EMPTY!";
+        cout << "\nThe Current size is: " << QueueDouble.Size() << endl;
+    }
+
+    QueueDouble.Enqueue(2.1);
+    QueueDouble.PrintQueue();
+
+    // Display the front most item in queue.
+    cout << "\nThe Front item is: " << QueueDouble.Front() << endl;
+
+    QueueDouble.Enqueue(3.3);
+    QueueDouble.PrintQueue();
+    QueueDouble.Enqueue(4.4);
+    QueueDouble.PrintQueue();
+    QueueDouble.Enqueue(5.5);
+    QueueDouble.PrintQueue();
+    QueueDouble.Enqueue(6.6);
+    QueueDouble.PrintQueue();
+    QueueDouble.Enqueue(7.7);
+    QueueDouble.PrintQueue();
+
+    // Double Dequeue's (1 Total)
+    QueueDouble.Dequeue();
+    QueueDouble.PrintQueue();
+
+    // Double Enqueue's (2 Total)
+    QueueDouble.Enqueue(8.8);
+    QueueDouble.PrintQueue();
+    QueueDouble.Enqueue(9.9);
+    QueueDouble.PrintQueue();
+
+    // Double Dequeue's (5 Total)
+    QueueDouble.Dequeue();
+    QueueDouble.PrintQueue();
+    QueueDouble.Dequeue();
+    QueueDouble.PrintQueue();
+    QueueDouble.Dequeue();
+    QueueDouble.PrintQueue();
+    QueueDouble.Dequeue();
+    QueueDouble.PrintQueue();
+    QueueDouble.Dequeue();
+    QueueDouble.PrintQueue();
+
+    // Check if Queue is full using IsFull() function. (FAIL CONDITION)
+    if(QueueDouble.IsFull()){
+        cout << "\nQueue is FULL!!";
+        cout << "\nThe Current size is: " << QueueDouble.Size();
+    } else {
+        cout << "\nQueue is NOT FULL!";
+        cout << "\nThe Current size is: " << QueueDouble.Size();
+    }
+
+    cout << "\n\nChecking IsFull() Queue class function...\n";
+
+    QueueDouble.Enqueue(999);
+    QueueDouble.PrintQueue();
+    QueueDouble.Enqueue(999);
+    QueueDouble.PrintQueue();
+    QueueDouble.Enqueue(999);
+    QueueDouble.PrintQueue();
+    QueueDouble.Enqueue(999);
+    QueueDouble.PrintQueue();
+    QueueDouble.Enqueue(999);
+    QueueDouble.PrintQueue();
+    QueueDouble.Enqueue(999);
+    QueueDouble.PrintQueue();
+    QueueDouble.Enqueue(999);
+    QueueDouble.PrintQueue();
+    QueueDouble.Enqueue(999);
+    QueueDouble.PrintQueue();
+    QueueDouble.Enqueue(999);
+    QueueDouble.PrintQueue();
+
+    // Check if Queue is full using IsFull() function. (PASS CONDITION)
+    if(QueueDouble.IsFull()){
+        cout << "\nQueue is FULL!!";
+        cout << "\nThe Current size is: " << QueueDouble.Size() << endl;
+    } else {
+        cout << "\nQueue is NOT FULL!";
+        cout << "\nThe Current size is: " << QueueDouble.Size() << endl;
+    }
+
+    // Testing Copy-Constructor with Double Type Queue.
+    cout << "\n\nTesting Copy Constructor for Queue Class...\n";
+    Queue <double> newQueueDouble(QueueDouble);
+    newQueueDouble.PrintQueue();
 
     return 0;
 }
